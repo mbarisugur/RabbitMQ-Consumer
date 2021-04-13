a6e6c4d3537992a076ea8b0613870b01fbee1118
Mustafa Barış Uğur <73464837+mbarisugur@users.noreply.github.com>
2021-04-13 12:16:45 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.Consumer
+{
+    public enum LogNames
+    {
+        Critical,
+        Error,
+        Info,
+        Warning
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqps://klcrkeba:JnfCJH6Kdo4H2rNQ9iM9GO-8u71VjOHv@coyote.rmq.cloudamqp.com/klcrkeba")
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("topic_exchange", durable: true, type: ExchangeType.Topic);
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    string routingKey = "#.Warning";
+
+                    channel.QueueBind(queue: queueName, exchange: "topic_exchange", routingKey: routingKey);
+
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, false);
+
+                    Console.WriteLine("Custom log bekliyorum....");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    channel.BasicConsume(queueName, false, consumer);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var log = Encoding.UTF8.GetString(ea.Body.ToArray());
+                        Console.WriteLine("log alındı:" + log);
+
+                        int time = int.Parse(GetMessage(args));
+                        Thread.Sleep(time);
+
+                        File.AppendAllText("logs_critical_error.txt", log + "\n");
+
+                        Console.WriteLine("loglama bitti");
+
+                        channel.BasicAck(ea.DeliveryTag, multiple: false);
+                    };
+                    Console.WriteLine("Çıkış yapmak tıklayınız..");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args[0].ToString();
+        }
+    }
+}
